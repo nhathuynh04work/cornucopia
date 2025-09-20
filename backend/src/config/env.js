af1deede6394eee16e746a40e3409e193fa659ef
Anh Nhật <nhathuynh04work@gmail.com>
2025-09-20 11:31:37 +0700

    Add connection string to env, set up config/db.js, test db connection

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 config();
 
 export const env = {
-	PORT: process.env.PORT,
-	FRONTEND_URL: process.env.FRONTEND_URL,
+    PORT: process.env.PORT,
+    FRONTEND_URL: process.env.FRONTEND_URL,
+    DB_CONNECTION_STRING: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 };
