89cfd6d18874ae4cda8436ec3c14ee5bc1451881
Anh Nhật <nhathuynh04work@gmail.com>
2025-09-30 10:25:30 +0700

    refractor question nav item

--- frontend/src/components/QuestionNavItem.jsx
+++ frontend/src/components/QuestionNavItem.jsx
@@ -1,16 +1,8 @@
-import { useLocation } from "react-router";
-
 function QuestionNavItem({ children, id }) {
-	const { hash } = useLocation();
-	const targetHash = `#question-${id}`;
-	const isActive = hash === targetHash;
-
 	return (
 		<a
-			href={targetHash}
-			className={`border flex justify-center items-center text-sm aspect-square ${
-				isActive ? "bg-blue-500 text-white" : ""
-			}`}>
+			href={`#question-${id}`}
+			className={`border flex justify-center items-center text-sm aspect-square `}>
 			{children}
 		</a>
 	);
