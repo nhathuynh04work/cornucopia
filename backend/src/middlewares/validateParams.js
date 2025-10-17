e00a7e760470fc680cd7165978316b82590e87ff
Anh Nhật <nhathuynh04work@gmail.com>
2025-10-17 20:36:31 +0700

    add validate params middleware

---
+++ backend/src/middlewares/validateParams.js
@@ -0,0 +1,13 @@
+export const validateParams = (paramNames) => (req, res, next) => {
+	for (const name of paramNames) {
+		const paramValue = req.params[name];
+		const id = parseInt(paramValue, 10);
+		if (isNaN(id) || id < 1) {
+			return res.status(400).json({
+				message: `Invalid parameter. '${name}' must be a positive integer.`,
+			});
+		}
+	}
+
+	return next();
+};
