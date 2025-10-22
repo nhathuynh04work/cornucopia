cb864727ac9e71a9b0534c6798b6641dac5d636f
Anh Nhật <nhathuynh04work@gmail.com>
2025-10-22 12:35:27 +0700

    add change layout on last media delete trigger

--- backend/src/repositories/test.repository.js
+++ backend/src/repositories/test.repository.js
@@ -11,7 +11,7 @@ export async function create(data, client = prisma) {
 			...data,
 			items: {
 				create: {
-					type: itemTypeEnum.MULTIPLE_CHOICE,
+					type: itemTypeEnum.SHORT_ANSWER,
 				},
 			},
 		},
