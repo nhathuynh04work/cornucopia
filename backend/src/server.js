71360ea864809e92a0f97c1d4c641cc72ea9674d
Anh Nhật <nhathuynh04work@gmail.com>
2025-10-22 21:19:11 +0700

    implement create attempt route

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,8 @@ import {
 	cardRouter,
 	listRouter,
 	sessionRouter,
-    mediaRouter,
+	mediaRouter,
+	attemptRouter,
 } from "./routes/index.js";
 import passport from "./config/passport.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
@@ -44,6 +45,7 @@ app.use("/tests", testRouter);
 app.use("/items", itemRouter);
 app.use("/options", optionRouter);
 app.use("/sessions", sessionRouter);
+app.use("/attempts", attemptRouter);
 
 // Handler riêng cho payload quá lớn (413)
 app.use((err, req, res, next) => {
