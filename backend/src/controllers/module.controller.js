13c02fdd444b9270ba495f87ec8ce3decfddd5ff
Anh Nhật <nhathuynh04work@gmail.com>
2025-10-30 11:27:36 +0700

    backend route for updating module info

--- backend/src/controllers/module.controller.js
+++ backend/src/controllers/module.controller.js
@@ -1,5 +1,12 @@
 import * as moduleService from "../services/module.service.js";
 
+export async function updateModule(req, res) {
+	const id = req.params.id;
+	const module = await moduleService.update(id, req.body);
+
+	res.status(200).json({ module });
+}
+
 export async function deleteModule(req, res) {
 	const id = req.params.id;
 	await moduleService.remove(id);
