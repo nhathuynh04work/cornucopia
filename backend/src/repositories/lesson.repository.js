5ec7fb92f1ef779ac14fed2a51a415cb550cbc89
Anh Nhật <nhathuynh04work@gmail.com>
2025-10-31 15:10:19 +0700

    backend route to update lesson

--- backend/src/repositories/lesson.repository.js
+++ backend/src/repositories/lesson.repository.js
@@ -1,5 +1,13 @@
 import prisma from "../prisma.js";
 
+export async function findById(id, client = prisma) {
+	return client.lesson.findUnique({ where: { id } });
+}
+
 export async function create(data, client = prisma) {
 	return client.lesson.create({ data });
+}
+
+export async function update(id, data, client = prisma) {
+	return client.lesson.update({ where: { id }, data });
 }
---
+++ backend/src/schemas/lesson.schema.js
@@ -0,0 +1,12 @@
+import z from "zod";
+import { LessonType } from "../generated/prisma/index.js";
+
+const LessonTypeSchema = z.enum([LessonType.TEXT, LessonType.VIDEO]);
+
+const LessonSchema = z.object({
+	title: z.string(),
+	type: LessonTypeSchema.nullable(),
+	textContent: z.string().nullable(),
+});
+
+export const UpdateLessonSchema = LessonSchema.partial();
