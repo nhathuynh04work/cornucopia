11ee3c4354aecbcf2771dc9bf9bb06eb93cba713
Anh Nhật <nhathuynh04work@gmail.com>
2025-11-28 23:27:19 +0700

    fix checkout field mismatch

--- backend/src/media/media.controller.js
+++ backend/src/media/media.controller.js
@@ -20,27 +20,6 @@ const uploadFile = async (req, res) => {
 	});
 };
 
-const requestUploadURL = async (req, res) => {
-	const { uploadUrl, url } = await mediaService.generateUploadUrl(req.body);
-
-	res.status(200).json({ uploadUrl, url });
-};
-
-const setEntityProperty = async (req, res) => {
-	const userId = req.user.id;
-	const url = await mediaService.setEntityProperty({
-		...req.body,
-		userId,
-	});
-
-	res.status(200).json({ url });
-};
-
-const linkMediaToEntity = async (req, res) => {
-	const media = await mediaService.linkMediaToEntity(req.body);
-	res.status(201).json({ media });
-};
-
 const deleteMedia = async (req, res) => {
 	await mediaService.deleteMedia(req.params.id);
 	res.status(204).end();
@@ -48,8 +27,5 @@ const deleteMedia = async (req, res) => {
 
 export const mediaController = {
 	uploadFile,
-	requestUploadURL,
-	setEntityProperty,
-	linkMediaToEntity,
 	deleteMedia,
 };
